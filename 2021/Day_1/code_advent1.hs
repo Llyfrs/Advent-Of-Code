eaef96e84472f4a7d9b393c3debdc0a8909e2573
Llyfrs <k.dreiseitl@seznam.cz>
2021-12-05 16:26:11 +0100

    2021

---
+++ 2021/Day_1/code_advent1.hs
@@ -0,0 +1,15 @@
+import Text.Read (Lexeme(String))
+main = do  
+        contents <- readFile "input.txt"
+        print . count . map readInt . words $ contents
+-- alternately, main = print . map readInt . words =<< readFile "test.txt"
+
+readInt :: String -> Int
+readInt = read
+
+count [] = 0
+count [x] = 0
+count (x:y:xs) | x < y = 1 + count (y:xs) 
+               | otherwise = count (y:xs) 
+
+
