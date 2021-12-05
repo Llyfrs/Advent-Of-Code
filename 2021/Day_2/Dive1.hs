a22ddb3b01295bbffe047b09ecf60209a0b52308
Llyfrs <k.dreiseitl@seznam.cz>
2021-12-05 19:14:03 +0100

    Day_2_1

---
+++ 2021/Day_2/Dive1.hs
@@ -0,0 +1,23 @@
+import Text.Html (content)
+
+
+
+dive :: [String] -> Int -> Int -> Int
+dive [] x y = x * y 
+dive (line:lines) x y | action == "forward" = dive lines (x+read value) y
+                      | action == "up" = dive lines x (y-read value) 
+                      | action == "down" = dive lines x (y+read value)
+                      | otherwise = dive lines x y 
+                 where 
+                   action = (words line)!!0
+                   value =  (words line)!!1
+
+
+main = do  
+        contents <- readFile "input.txt"
+        let result = dive (lines contents) 0 0
+        putStrLn $ show result
+
+
+
+
