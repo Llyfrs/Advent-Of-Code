9707de408255124e0f48520b4499f9410bf74d83
llyfr <k.dreiseitl@seznam.cz>
2024-12-07 08:07:19 +0100

    Day_7 kinda proud of this one

---
+++ 2024/Day_7/src/main.rs
@@ -0,0 +1,90 @@
+
+use regex::Regex;
+
+
+enum Operation {
+    Add,
+    Multiply,
+    Combine
+}
+
+fn calculate(op: &Operation, a: u64, b: u64) -> u64 {
+    match op {
+        Operation::Add => a + b,
+        Operation::Multiply => a * b,
+        Operation::Combine => format!("{}{}", a, b).parse::<u64>().unwrap()
+    }
+}
+
+struct Test {
+    test_value: u64,
+    values : Vec<u64>,
+    operators: Vec<Operation>
+}
+
+fn load_input() -> Vec<Test> {
+    let string = std::fs::read_to_string("src/input.txt").unwrap();
+
+    let mut tests = Vec::new();
+    let re = Regex::new(r"(\d+)").unwrap();
+
+    for line in string.lines() {
+        let numbers: Vec<_> = re.captures_iter(line).map(|x| x[1].parse::<u64>().unwrap()).collect();
+        tests.push(Test { test_value: numbers[0], values: numbers[1..].to_vec(), operators: vec![]});
+    }
+    tests
+}
+
+impl Test {
+
+
+    fn is_valid(&self) -> bool {
+        self.test(self.values[0], self.values[1..].to_vec())
+    }
+
+    fn test(&self, acc: u64, left : Vec<u64>) -> bool {
+        if left.len() == 0 {
+            return acc == self.test_value
+        }
+
+        if acc > self.test_value {
+            return false
+        }
+
+        for op in &self.operators {
+            if self.test(calculate(op, acc, left[0]), left[1..].to_vec()) {
+                return true
+            }
+        }
+        false
+    }
+
+}
+
+
+fn main() {
+    let tests = load_input();
+
+    let mut sum = 0;
+    for mut test in tests {
+        test.operators = vec![Operation::Multiply, Operation::Add];
+        if test.is_valid(){
+            sum += test.test_value;
+        }
+    }
+
+    println!("Part 1: {sum}");
+
+    sum = 0;
+
+    let tests = load_input();
+    for mut test in tests {
+        test.operators = vec![Operation::Multiply,Operation::Combine,Operation::Add];
+        if test.is_valid() {
+            sum += test.test_value;
+        }
+    }
+
+    println!("Part 2: {sum}")
+
+}
